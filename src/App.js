76b6f084aa9b84bc84a94c8b2839d1f9f79b02d5
treatme030 <treatme8192@gmail.com>
2021-08-28 18:18:30 +0900

    dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    let amount = parseInt(count)// count가 문자타입으로 숫자로 변환
+    if(count <= 0){
+      amount = 1
+    }
+    if(count > data.length - 1){
+      amount = data.length
+    }
+    setText(data.slice(0, amount))
+  }
   return (
-    <div>
-      
-    </div>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count}
+        onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          {
+            text.map((item, index) => {
+              return <p key={index}>{item}</p>
+            })
+          }
+        </p>
+      </article>
+    </section>
   );
 }
 
